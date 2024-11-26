b0d2c330e646a23b921df7e20ff1f8ba611d2f43
Daniel Banks <danielnbanks@gmail.com>
2024-11-26 16:55:44 +0000

    Created Test decorators to test functionality

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,41 @@
 from python_publish_subscribe import PythonPublishSubscribe, test
 
-PythonPublishSubscribe = PythonPublishSubscribe()
-PythonPublishSubscribe.run()
+### WON'T BE PART OF THE FRAMEWORK, ONLY DOING THIS FOR QUICK TESTING
+from google.cloud import pubsub_v1
+
+PROJECT_ID = 'test-project-id'
+TOPIC_NAME = 'projects/{project_id}/topics/{topic}'.format(
+    project_id=PROJECT_ID,
+    topic='test-topic',  # Set this to something appropriate.
+)
+
+publisher = pubsub_v1.PublisherClient()
+
+try:
+    publisher.create_topic(name=TOPIC_NAME)
+    print(f"Topic '{TOPIC_NAME}' created in emulator.")
+except Exception as e:
+    print(f"Topic '{TOPIC_NAME}' already exists: {e}")
+
+###############################
+app = PythonPublishSubscribe()
+app.run()
+app.test_run()
+
+app.config.update('PUBLISH_TOPICS', TOPIC_NAME)
 
 test()
+
+@app.test_decorators
+def test_function():
+    print("foo but with no bar :(")
+
+@app.test_decorator_with_params("foo")
+def test_param():
+    print("bar")
+
+@app.test_map_function("hello_subscription")
+def test_call_function():
+    print("callback")
+
+test_param()
--- python_publish_subscribe/core.py
+++ python_publish_subscribe/core.py
@@ -1,10 +1,70 @@
+from threading import Thread
+from time import sleep
+
+from python_publish_subscribe.config import Config
+
 class PythonPublishSubscribe:
     def __init__(self, config=None):
         self.config = config or {}
         self.initialise()
+
+        self.test_func_map = {}
+
 
     def initialise(self):
         print("initialise")
 
     def run(self):
         print("run")
+
+
+    def add_subscription(self, callback, topic=None):
+        if not topic:
+            topic = callback.__name__
+        topic_dict = {
+            "topic": topic,
+            "callback": callback
+        }
+        self.config.add_value_to_key(Config.ConfigKeys.SUBSCRIPTION_TOPICS, topic_dict)
+
+
+    """
+    Used for testing to get a hand of and practice techniques
+    """
+
+    def test_decorators(self, test_fun):
+        test_fun()
+
+    def test_decorator_with_params(self, test_message):
+        def decorator(test_fun):
+            def wrapper(*args, **kwargs):
+                print(test_message)
+                return test_fun(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    def test_map_function(self, map_name):
+        def decorator(test_func):
+            self.test_func_map[map_name] = test_func
+            return test_func
+        return decorator
+
+    def test_call_function(self, map_name, *args, **kwargs):
+        if map_name in self.test_func_map:
+            return self.test_func_map[map_name](*args, **kwargs)
+
+    def test_run(self):
+        """
+        Not effiecent way of looping
+        and this is not how the final implementation will look
+        just doing it to work out how to call a function in a dict
+        :return:
+        """
+        print("run here")
+        def loop():
+            while True:
+                for map_name, function in self.test_func_map.items():
+                    self.test_call_function(map_name)
+                sleep(1)
+        thread = Thread(target=loop)
+        thread.start()
