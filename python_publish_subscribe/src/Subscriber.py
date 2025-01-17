1acdf34c60c40be124cdff292151e28b501c1f7b
Daniel Banks <danielnbanks@gmail.com>
2025-01-17 10:32:03 +0000

    Added basic subscriber

--- python_publish_subscribe/src/Subscriber.py
+++ python_publish_subscribe/src/Subscriber.py
@@ -1,3 +1,17 @@
+from google import pubsub_v1
+from google.auth.api_key import Credentials
+
+from python_publish_subscribe.config import Config
+
+
 class Subscriber:
-    def __init__(self):
-        pass
+    def __init__(self, config: Config, credentials: Credentials=None):
+        self._subscriber = pubsub_v1.SubscriberClient(credentials=credentials)
+        self._config = config
+
+
+    # def add_subscription(self, callback, subscription_name, topic_name):
+    #
+    #
+    # def start_listening(self):
+    #     with self._subscriber as subscriber:
